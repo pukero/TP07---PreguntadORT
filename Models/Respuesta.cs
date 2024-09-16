07d9bd146d01c1056271c90aab3e9b1f5ead483b
faculewko <162578943+faculewko@users.noreply.github.com>
2024-09-16 23:29:41 +0000

    avanzes views, falta un topque y estamos

--- Models/Respuesta.cs
+++ Models/Respuesta.cs
@@ -1,17 +1,19 @@
 public class Respuesta
 {
     public int IdRespuesta { get; set; }
+    public int IdPregunta { get; set; }
     public int Opcion {get; set;}
-    public string Nombre {get; set;}
+    public string Contenido {get; set;}
     public bool Correcta {get; set;}
-    public string Foto {get;set;} 
-    public Respuesta(){}
-    public Respuesta(int id_respuesta, int opcion, string nombre, bool correcta, string foto)
+ 
+  
+    public Respuesta(int id_respuesta, int opcion, string nombre, bool correcta, string contenido, int id_pregunta)
     {
         IdRespuesta=id_respuesta;
+        IdPregunta=id_pregunta;
         Opcion=opcion;
-        Nombre=nombre;  
+        Contenido=contenido;  
         Correcta=correcta;
-        Foto=foto;
+        
     }
 }
